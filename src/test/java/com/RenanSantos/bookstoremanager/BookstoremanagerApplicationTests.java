72879b7ddaaec703f68ec4ec1f3b7384bcc0acb6
Renan <renan.jobs13@gmail.com>
2021-03-21 18:40:12 +0000

    first commit : project structure created

---
+++ src/test/java/com/RenanSantos/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RenanSantos.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
